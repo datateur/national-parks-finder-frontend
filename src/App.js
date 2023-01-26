314604120f7dedbe0f55a8703d96128925d4e336
Jennifer <datateur@gmail.com>
2023-01-26 14:34:25 -0800

    Troubleshooting Google Maps API call. Map wont display on browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+let map;
+
+function initMap() {
+  map = new window.google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <main>
+      <div id="map"></div>
+      <p>hi there</p>
+      <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_zMsyhGAE1XHXQa7bT1B1sZph09pl-B4&callback=initMap"
+    ></script>
+      </main>
     </div>
   );
 }
