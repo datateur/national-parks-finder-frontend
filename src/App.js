be1ae242399a82b0a0315c162e3bfe7fde3a229e
Jennifer <datateur@gmail.com>
2023-01-30 23:31:23 -0800

    create and display map on browser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = window.L.map('map').setView([51.505, -0.09], 13);
+
+window.L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <main>
-      <div id="map"></div>
-      <p>hi there</p>
-      <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_zMsyhGAE1XHXQa7bT1B1sZph09pl-B4&callback=initMap"
-    ></script>
       </main>
     </div>
   );
